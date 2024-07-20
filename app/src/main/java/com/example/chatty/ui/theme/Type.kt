6bbbd6aef6688b14cf1496f0ff3f4d59a42cd117
PythonVader <youssefiyd10@gmail.com>
2024-07-20 17:54:01 +0300

    Added Color Pallet and Theming

--- app/src/main/java/com/example/chatty/ui/theme/Type.kt
+++ app/src/main/java/com/example/chatty/ui/theme/Type.kt
@@ -2,9 +2,13 @@ package com.example.chatty.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.example.chatty.R
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
